d7281deeb44a14ca8eef8caeb5b134eebf1c0daa
DESKTOP-EE6KA0I\REDTABLE <kkj214@gmail.com>
2022-11-15 10:38:40 +0900

    프로그래머스 문제풀이

---
+++ Programmers/Level_3/이중우선큐.py
@@ -0,0 +1,14 @@
+def solution(operations):
+    answer = []
+    for i in operations :
+        if i[0] == 'I' :
+            answer.append(int(i[2:]))
+        elif i[0] == 'D' and len(answer) > 0  : 
+            if i[2].isdigit() :
+                answer.remove(max(answer))
+            else :
+                answer.remove(min(answer))
+    if len(answer) > 1 :
+        return [max(answer),min(answer)]
+    else :
+        return [0,0]
