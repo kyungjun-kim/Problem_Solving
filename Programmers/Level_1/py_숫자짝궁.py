fe2ef6cc732044c9828b9aad5926bf7a2345b40e
DESKTOP-EE6KA0I\REDTABLE <kkj214@gmail.com>
2022-12-12 11:51:41 +0900

    문제 풀이

--- Programmers/Level_1/py_숫자짝궁.py
+++ Programmers/Level_1/py_숫자짝궁.py
@@ -1,16 +1,13 @@
 # 문제 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/131128
 
 def solution(X, Y):
-    result = []
-    num = list(map(lambda x : str(x), range(10)))
-    for i in num :
+    result = ''
+    for i in list(map(lambda x : str(x), range(9,-1,-1))) :
         if i in X and i in Y :
-            tmp = [i]
-            result.extend(tmp * min(X.count(i), Y.count(i)))
-            X,Y = X.replace(i,'',1), Y.replace(i,'',1)
-    if result == [] :
+            result += i * min(X.count(i), Y.count(i))
+    if result == '' :
         return "-1"
     elif set(result) == {'0'} :
         return "0"
     else :
-        return ''.join(sorted(result, reverse = True))
+        return result 
