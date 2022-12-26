ca4dc841909127188a9c1ef0f359278587a6c4ca
DESKTOP-EE6KA0I\REDTABLE <kkj214@gmail.com>
2022-12-26 18:14:37 +0900

    문제 풀이

---
+++ Programmers/Level_1/py_크기가 작은 부분 문자열.py
@@ -0,0 +1,8 @@
+# 문제 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/147355
+
+def solution(t, p):
+    answer = 0
+    for i in range(len(t)-len(p)+1) :
+        if int(t[i:i+len(p)]) <= int(p) :
+            answer += 1
+    return answer
