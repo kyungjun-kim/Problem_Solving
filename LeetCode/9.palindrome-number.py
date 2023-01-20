c53bd96108c9d597f2bb46be5e87fa332b7e4c25
DESKTOP-EE6KA0I\REDTABLE <kkj214@gmail.com>
2023-01-20 12:24:07 +0900

    문제풀이

---
+++ LeetCode/9.palindrome-number.py
@@ -0,0 +1,8 @@
+# 문제 링크 : https://leetcode.com/problems/palindrome-number/
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if str(x) == str(x)[::-1] :
+            return True
+        else :
+            return False
