62e06ddb436517dd6eed2b2c3234130d017f0f07
KyungJun_Kim <58836568+kyungjun-kim@users.noreply.github.com>
2024-04-16 16:20:04 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.py
@@ -0,0 +1,6 @@
+def solution(numbers):
+    nd = {"zero" : "0", "one": "1", "two" : "2", "three" : "3", "four" : "4", "five" : 
+"5", "six" : "6", "seven" : "7", "eight" : "8", "nine" : "9"}
+    for i in nd :
+        numbers = numbers.replace(i,nd[i])
+    return int(numbers)
